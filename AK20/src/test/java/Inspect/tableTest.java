d02d8d5cec54f1f5b0519136d6cf5c50d20d5f62
tphuong <phuongpt1611@gmail.com>
2019-03-18 16:25:03 +0700

    test

---
+++ AK20/src/test/java/Inspect/tableTest.java
@@ -0,0 +1,51 @@
+package Inspect;
+
+import static support.CommonFunctions.*;
+
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.How;
+import support.Browsers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class tableTest {
+    public static void main(String[] args) {
+
+        openBrowser(Browsers.CHROME);
+        navigate("https://the-internet.herokuapp.com/tables");
+        List<WebElement> rows = getElements(How.XPATH, "//table[@id='table1']/tbody/tr");
+        List<WebElement> columns = getElements(How.XPATH, "//table[@id='table1']/thead/tr/th");
+
+        String cellLocator = "//table[@id='table1']/tbody/tr[%d]/td[%d]";
+
+        List<List<String>> dataRowList = new ArrayList<>();
+
+
+        for (int i = 1; i <= rows.size(); i++) {
+            List<String> cellList = new ArrayList<>();
+            for (int j = 1; j <= columns.size(); j++) {
+
+                cellList.add(getElement(How.XPATH, String.format(cellLocator, i, j)).getText());
+                //System.out.println(stringList.get(stringList.size() - 1));
+            }
+            dataRowList.add(cellList);
+        }
+
+      for (List<String> cellList:dataRowList)
+      {
+          //for (String cellValue:cellList)
+          //{
+              //if (cellValue.contains("John"))
+          if (cellList.get(1).contains("John"))
+          {
+                  System.out.println(cellList);
+              }
+          //}
+      }
+        driver.quit();
+
+        }
+
+    }
